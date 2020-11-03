ffa3f1d971195657da0803354f5d20611155153f
Royce <random.emailx828@gmail.com>
2020-11-03 17:23:28 -0600

    UI

---
+++ Assets/UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+}
