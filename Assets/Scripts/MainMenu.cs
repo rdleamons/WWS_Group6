79c88876bb01c5a9865ec58438684be3ce28ad1e
Rebekah <rdleamons@gmail.com>
2020-11-03 17:51:16 -0600

    Menu updated, art added
    Pre-class push (11/3)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button newGameButton;
+
+    public Button creditsButton;
+
+    public Button htpButton;
+
+    public Button quitButton;
+
+    public void NewGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void HowtoPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
