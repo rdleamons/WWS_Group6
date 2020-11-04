01dd9468c61fe48b67807eddd388c318b4292ef3
Rebekah <rdleamons@gmail.com>
2020-11-04 13:32:22 -0600

    Win Scene

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    public Button restart;
+    public Button quitButton;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
